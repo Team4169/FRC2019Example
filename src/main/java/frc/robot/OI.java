0a53186395832c758b1959dd8df44b77f3913edf
jhuang364 <jason.huang.sudbury@gmail.com>
2018-11-27 16:18:03 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,12 +1,40 @@
 import edu.wpi.first.wpilibj.XboxController;
 
-public class OI {
-    public OI() {
-        // TODO
-    }
+package frc.robot;
 
-    public XBoxController getController() {
-        // TODO
-        return null;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  XboxController controller1 = new XboxController(0);
+  XboxController controller2 = new XboxController(1);
+  double getController1X(Hand hand) {
+    if(Math.abs(controller1.getX(hand)) <= 0.2) {
+      return 0;
     }
+    else return controller1.getX(hand);
+  }
+
+  double getController1Y() {
+    if(Math.abs(controller1.getY(hand)) <= 0.2) {
+      return 0;
+    }
+    else return controller1.getY(hand);
+  }
+  double getController2X(Hand hand) {
+    if(Math.abs(controller2.getX(hand)) <= 0.2) {
+      return 0;
+    }
+    else return controller2.getX(hand);
+  }
+
+  double getController2Y() {
+    if(Math.abs(controller2.getY(hand)) <= 0.2) {
+      return 0;
+    }
+    else return controller2.getY(hand);
+  }
 }
