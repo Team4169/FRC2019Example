46c011c3070746b6cf608218a521a2c8a9f61487
jhuang364 <jason.huang.sudbury@gmail.com>
2018-12-04 15:59:50 -0500

    Fixes

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,15 +2,11 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID;
 package frc.robot;
 
+
 /**
 * Add your docs here.
 */
 public class OI {
-HashMap<String, Integer> buttons = new HashMap<String, Integer>();
-  buttons.put("A", 0);
-  buttons.put("B", 1);
-  buttons.put("X", 2);
-  buttons.put("Y", 3);
  public static final double DEAD_ZONE = 0.2;
  // Put methods for controlling this subsystem
  // here. Call these from Commands.
@@ -23,31 +19,57 @@ HashMap<String, Integer> buttons = new HashMap<String, Integer>();
      else return false;
  }
 
- double getController1X() {
-   if(isDead(controller1.getX(), controller1.getY())) {
+ double getController1XLeft() {
+   if(isDead(controller1.getX(GenericHID.Hand.kLeft), controller1.getY(GenericHID.Hand.kLeft))) {
      return 0;
    }
-   else return controller1.getX();
+   else return controller1.getX(GenericHID.Hand.kLeft);
  }
 
- double getController1Y() {
-   if(isDead(controller1.getX(), controller1.getY())) {
+ double getController1YLeft() {
+   if(isDead(controller1.getX(GenericHID.Hand.kLeft), controller1.getY(GenericHID.Hand.kLeft))) {
      return 0;
    }
-   else return controller1.getY();
+   else return controller1.getY(GenericHID.Hand.kLeft);
  }
- double getController2X() {
-   if(isDead(controller1.getX(), controller1.getY())) {
+ double getController2XLeft() {
+   if(isDead(controller1.getX(GenericHID.Hand.kLeft), controller1.getY(GenericHID.Hand.kLeft))) {
      return 0;
    }
-   else return controller2.getX();
+   else return controller2.getX(GenericHID.Hand.kLeft);
  }
 
- double getController2Y() {
-   if(isDead(controller1.getX(), controller1.getY())) {
+ double getController2YLeft() {
+   if(isDead(controller1.getX(GenericHID.Hand.kLeft), controller1.getY(GenericHID.Hand.kLeft))) {
      return 0;
    }
-   else return controller2.getY();
+   else return controller2.getY(GenericHID.Hand.kLeft);
+ }
+ double getController1XRight() {
+   if(isDead(controller1.getX(GenericHID.Hand.kRight), controller1.getY(GenericHID.Hand.kRight))) {
+     return 0;
+   }
+   else return controller1.getX(GenericHID.Hand.kRight);
+ }
+
+ double getController1YRight() {
+   if(isDead(controller1.getX(GenericHID.Hand.kRight), controller1.getY(GenericHID.Hand.kRight))) {
+     return 0;
+   }
+   else return controller1.getY(GenericHID.Hand.kRight);
+ }
+ double getController2XRight() {
+   if(isDead(controller1.getX(GenericHID.Hand.kRight), controller1.getY(GenericHID.Hand.kRight))) {
+     return 0;
+   }
+   else return controller2.getX(GenericHID.Hand.kRight;
+ }
+
+ double getController2YRight() {
+   if(isDead(controller1.getX(GenericHID.Hand.kRight), controller1.getY(GenericHID.Hand.kRight))) {
+     return 0;
+   }
+   else return controller2.getY(GenericHID.Hand.kRight);
  }
 }
 
