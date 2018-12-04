8c8cabffc0d43f458f61c75cfc52fa1a6740d232
jhuang364 <jason.huang.sudbury@gmail.com>
2018-12-04 15:23:29 -0500

    OI.java
    Changes to Controller

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,12 +1,47 @@
 import edu.wpi.first.wpilibj.XboxController;
+package frc.robot;
 
+
+/**
+ * Add your docs here.
+ */
 public class OI {
-    public OI() {
-        // TODO
+  public static final int DEAD_ZONE = 0.2;
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  XboxController controller1 = new XboxController(0);
+  XboxController controller2 = new XboxController(1);
+  boolean isDead(double x, double y) {
+    if(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) < DEAD_ZONE){
+      return true;
     }
+      else return false;
+  }
 
-    public XBoxController getController() {
-        // TODO
-        return null;
+  double getController1X(Hand hand) {
+    if(isDead(controller1.getX(hand), controller1.getY(hand))) {
+      return 0;
     }
+    else return controller1.getX(hand);
+  }
+
+  double getController1Y() {
+    if(isDead(controller1.getX(hand), controller1.getY(hand))) {
+      return 0;
+    }
+    else return controller1.getY(hand);
+  }
+  double getController2X(Hand hand) {
+    if(isDead(controller1.getX(hand), controller1.getY(hand))) {
+      return 0;
+    }
+    else return controller2.getX(hand);
+  }
+
+  double getController2Y() {
+    if(isDead(controller1.getX(hand), controller1.getY(hand))) {
+      return 0;
+    }
+    else return controller2.getY(hand);
+  }
 }
