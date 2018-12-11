73813e4d36056daa84702e1cef3288bf5213a1c5
Spencerbot15 <24553779+Spencerbot15@users.noreply.github.com>
2018-12-11 15:46:05 -0500

    Update DriveForTime.java

--- src/main/java/frc/robot/DriveForTime.java
+++ src/main/java/frc/robot/DriveForTime.java
@@ -7,12 +7,14 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.TimedCommand;
 
-public class DriveForTime extends Command {
-  public DriveForTime() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+public class DriveForTime extends TimedCommand {
+  private final double power;
+
+  public DriveForTime(double power, double time, String name) {
+    super(name, time);
+    this.power = power;
   }
 
   // Called just before this Command runs the first time
@@ -23,12 +25,7 @@ public class DriveForTime extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    return false;
+    Robot.driveTrain.setPower(this.power, this.power);
   }
 
   // Called once after isFinished returns true
